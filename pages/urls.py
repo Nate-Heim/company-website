6504b479f57a0c93649ddf5290978d6307ee12c7
Nate Heim <heim.nathanb@gmail.com>
2024-09-25 11:29:39 -0600

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,14 @@
+# pages/urls.py
+
+from django.urls import path
+
+from .views import home_page_view, AboutPageView  # new
+
+urlpatterns = [
+    path(
+        "about/",
+        AboutPageView.as_view(),
+        name="about",
+    ),  # new
+    path("", home_page_view, name="home"),
+]
---
+++ pages/views.py
@@ -0,0 +1,24 @@
+# pages/views.py
+
+from django.shortcuts import render
+from django.views.generic import TemplateView  # new
+
+
+# Create your views here.
+def home_page_view(request):
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greetings": "THAnk you FOR visitING",
+    }
+
+    return render(request, "home.html", context)
+
+
+class AboutPageView(TemplateView):  # new
+    template_name = "about.html"  # new
+
+    def get_context_data(self, **kwargs):  # new
+        context = super().get_context_data(**kwargs)  # new
+        context["contact_address"] = "1600 Pennsylvania Ave, Washington DC, DC 20500"
+        context["phone_number"] = "202 208 1631"
+        return context
